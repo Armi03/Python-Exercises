bdf83938d24b8f0e5c27e4f8bc2960a80dafe4b1
Armi Amipara <Armi.Amipara@metropolia.fi>
2024-10-24 14:15:50 +0300

    Exercises

---
+++ Exercise-git.py
@@ -0,0 +1,3 @@
+
+name = "Armi Amipara"
+print(f"Hello, {name}!")
---
+++ Lecture01-Exercise.py
@@ -0,0 +1,30 @@
+#01 Program to welcome the user
+name = input("Please enter your name: ")
+print(f"Welcome, {name}!")
+
+#02 Program to compute gross pay
+hours = float(input("Enter hours worked: "))
+rate_per_hour = float(input("Enter rate per hour: "))
+gross_pay = hours * rate_per_hour
+print(f"Gross pay: {gross_pay}")
+
+#04 Program to convert Celsius to Fahrenheit
+# Assignment statements
+width = 17
+height = 12.0
+
+# Evaluating the expressions
+
+a = width//2
+print(a)
+
+b = width/2.0
+print(b)
+
+c = height/3
+print(c)
+
+#04 Program to convert Celsius to Fahrenheit
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (9/5) * celsius + 32
+print(f"Temperature in Fahrenheit: {fahrenheit:.3f}")
